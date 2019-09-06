e9a8e59df72172ccd71a5d6a3dac79cb9d40adb2
Sarah <Sarah_Bani@outlook.com>
2019-09-06 22:16:20 +0430

    Add project files.

---
+++ Core/Core.DomainModel/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Core.DomainModel
+{
+    public class Class1
+    {
+    }
+}
---
+++ Core/Core.DomainService/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Core.DomainService
+{
+    public class Class1
+    {
+    }
+}
