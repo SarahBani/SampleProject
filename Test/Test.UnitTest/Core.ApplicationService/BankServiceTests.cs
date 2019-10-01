54aac43b516a7ba2da13e45127756171fea52f5e
Sarah <Sarah_Bani@outlook.com>
2019-10-01 20:40:54 +0330

    Unit test for checking Bank duplicate record

--- Test/Test.UnitTest/Core.ApplicationService/BankServiceTests.cs
+++ Test/Test.UnitTest/Core.ApplicationService/BankServiceTests.cs
@@ -1,9 +1,14 @@
 ﻿using Core.ApplicationService.Implementation;
+using Core.DomainModel;
 using Core.DomainModel.Entities;
+using Core.DomainServices;
 using Core.DomainServices.Repositoy;
+using Moq;
 using NUnit.Framework;
 using System.Collections.Generic;
+using Test.UnitTest.Common;
 using Test.UnitTest.Common.Models;
+using Test.UnitTest.Infrastructure.Common;
 
 namespace Test.UnitTest.Core.ApplicationService
 {
@@ -45,6 +50,26 @@ namespace Test.UnitTest.Core.ApplicationService
         public override void Setup()
         {
             base.SetService<BankService>();
+        }
+
+        [Test]
+        public void InsertAsync_DuplicateName_ReturnsDuplicateException()
+        {
+            // Arrange
+            var entity = Entity;
+            var exceptionKey = ExceptionKey.RecordAlreadyExsits;
+            var expectedResult = new TransactionResult(new CustomException(exceptionKey));
+            base.RepositoryMock.Setup(q => q.InsertAsync(It.IsAny<Bank>()))
+                .Throws(SqlExceptionGenerator.CreateSqlException((int)exceptionKey));
+
+            //Act
+            var result = this.Service.InsertAsync(entity).Result;
+
+            // Assert
+            Assert.IsInstanceOf<TransactionResult>(result);
+            base.RepositoryMock.Verify(q => q.InsertAsync(It.IsAny<Bank>()),
+                "error in calling the correct method");  // Verifies that Repository.InsertAsync was called
+            AssertHelper.AreEqualEntities(expectedResult, result, "error in raising the correct exception");
         }
 
         #endregion /Methods
