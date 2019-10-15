0f0fd83a670b6737d230e1c15931de74efc11c6c
Sarah <Sarah_Bani@outlook.com>
2019-10-15 13:09:36 +0330

    Tests corrections

---
+++ Test/Test.Common/Models/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test.Common.Models
+{
+    public interface BaseModel<TEntity>
+    {
+
+        TEntity Entity { get; }
+
+        IList<TEntity> EntityList { get; }
+
+    }
+}
